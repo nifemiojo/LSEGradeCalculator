d343134939bc2f51c8fbc0fd97c4f50fc47e6855
Femi Ojo <nifemiojo13@yahoo.co.uk>
2020-12-18 12:22:37 +0000

    removed build from git ignore

---
+++ build/precache-manifest.24a0475c4318c08667461845cdd3d8f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0841bb9761516c46e75996d8f540aa01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6946cecd075dbf3fc182",
+    "url": "/static/css/main.81f7151b.chunk.css"
+  },
+  {
+    "revision": "3abee0311b8fce26f677",
+    "url": "/static/js/2.93a4e039.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.93a4e039.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6946cecd075dbf3fc182",
+    "url": "/static/js/main.f7f48cf5.chunk.js"
+  },
+  {
+    "revision": "7be5274189c66e5447d6",
+    "url": "/static/js/runtime-main.4a80414a.js"
+  },
+  {
+    "revision": "48f2b86e2e67f7a9d6a6c1c977b5b73a",
+    "url": "/static/media/Agrandir-Regular.48f2b86e.woff"
+  }
+]);
